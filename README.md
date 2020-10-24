6a8a2fc22b4032486980535f40e2760fa20a877f
ShevchenkoRostyslav <shevchenko.rostislav@gmail.com>
2020-10-24 23:05:17 +0200

    - update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# RockPaperScissor
- Building an interactive Rock-Paper-Scissor computer game using OpenCV + Tensorflow 2.0
+# Building an interactive Rock-Paper-Scissor game using OpenCV + Tensorflow 2.0
+
+To play 3 rounds of the game:
+
+```
+python play.py --rounds 3
+```
+Details about how to generate the training data and train the DNN model can be found below. 
+## Generate the training data
+
+To generate the training data for any of labels: rock/scissor/paper, run:
+```
+python genLabeledData.py --label rock 
+```
+To quite the generator press and hold ``q`` or provide the ``--max_images`` argument.
+
+## Model training
+
--- play.py
+++ play.py
@@ -1,3 +1,6 @@
+import argparse
+from typing import Dict
+
 from src.game import RockPaperScissorGame, User, Bot
 import tensorflow.keras as K
 import logging
@@ -5,10 +8,28 @@ import logging
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
+def parse_args() -> Dict:
+    """Input argument parser
 
-if __name__ == '__main__':
+    :return:
+    """
+    parser = argparse.ArgumentParser(description='Play the rock-paper-scissor game using the web-camera')
+    parser.add_argument('--rounds', type=int, help='Number of rounds to be played', required=True)
+    args = parser.parse_args()
+    return vars(args)
+
+
+def initialize_game(rounds: int) -> RockPaperScissorGame:
+    # load the trained model weights
     trained_model = K.models.load_model('model_weights/model.h5')
+    # initialize the players: Bot and User
     user = User(trained_model)
     bot = Bot()
-    game = RockPaperScissorGame(user, bot, 3)
+    game = RockPaperScissorGame(user, bot, rounds)
+    return game
+
+
+if __name__ == '__main__':
+    in_args = parse_args()
+    game = initialize_game(in_args['rounds'])
     game.play()
